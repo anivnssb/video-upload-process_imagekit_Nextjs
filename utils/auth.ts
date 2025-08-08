36fc4a030f5c9888ebd98bf4691b495594a03b14
anvinps <anvinssb@gmail.com>
2025-08-08 22:19:43 +0530

    custom credential provider authorization logic

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,6 +1,10 @@
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { dbConnect } from "./db";
+import User from "@/model/user";
+import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -11,6 +15,36 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Missing Email or password");
+        }
+        try {
+          await dbConnect();
+          const user = await User.findOne({ email: credentials.email });
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isValid) {
+            throw new Error("Invalid password");
+          }
+          return { id: user._id.toString(), email: user.email };
+        } catch (error) {
+          console.error("Auth Error:", error);
+          throw error;
+        }
+      },
     }),
   ],
 };
